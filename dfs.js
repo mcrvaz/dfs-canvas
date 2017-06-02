f6b3db90458804884f0d7231073b468ac1274f28
Marcello Vaz <marcello.vaz@finxi.com.br>
2017-06-02 12:08:16 -0300

    Geração aleatoria de nós

--- dfs.js
+++ dfs.js
@@ -1,49 +1,57 @@
 (() => {
+
     class Node {
-        constructor(x,y){
+
+        constructor(x, y, radius){
             this.x = x;
             this.y = y;
+        }
+
+        static get active() { return "white" };
+        static get inactive() { return "green" };
+
+        draw(canvas, context, color){
+            let radius = 5;
+            context.beginPath();
+            context.arc(this.x * canvas.width, this.y * canvas.height, radius, 0, 2 * Math.PI, false);
+            context.fillStyle = color;
+            context.fill();
+            context.lineWidth = 2;
+            context.strokeStyle = 'black';
+            context.stroke();
         }
     }
 
     class Main {
-        constructor(canvas, context){
+        constructor(canvas, context, qttNodes){
             this.canvas = canvas;
             this.context = context;
-            this.canvas.width = 1000;
-            this.canvas.height = 300;
+            this.qttNodes = qttNodes;
+            this.canvas.width = 1350;
+            this.canvas.height = 400;
         }
 
         run(){
-            this.drawCircle(Math.random(), Math.random());
-            this.drawCircle(Math.random(), Math.random());
-            this.drawCircle(Math.random(), Math.random());
-            this.drawCircle(Math.random(), Math.random());
-            this.drawCircle(Math.random(), Math.random());
-            this.drawCircle(Math.random(), Math.random());
-            this.drawCircle(Math.random(), Math.random());
+            let nodes = this.generateNodes(this.qttNodes);
+            nodes.forEach((n) => { n.draw(this.canvas, this.context, Node.active) });
         }
 
-        drawCircle(x, y){
-            let radius = 10;
-            this.context.beginPath();
-            this.context.arc(x * this.canvas.width, y * this.canvas.height, radius, 0, 2 * Math.PI, false);
-            this.context.fillStyle = 'green';
-            this.context.fill();
-            this.context.lineWidth = 5;
-            this.context.strokeStyle = 'black';
-            this.context.stroke();
+        depthFirstSearch(){}
+
+        generateNodes(qtt) {
+            return Array.from(Array(qtt), () => new Node(Math.random(), Math.random()));
         }
 
-         getDistance(nodeA, nodeB){
-            return Math.sqrt(Math.pow((nodeA.x - nodeB.x),2) + Math.pow((nodeA.y - nodeB.y),2));
+         getDistance(n1, n2){
+            return Math.sqrt(Math.pow((n1.x - n2.x),2) + Math.pow((n1.y - n2.y),2));
         }
 
     }
 
     let canvas = document.getElementById("main-canvas");
     let context = canvas.getContext("2d");
-    let main = new Main(canvas, context);
+    let qttNodes = 1000;
+    let main = new Main(canvas, context, qttNodes);
     main.run();
 
 })();
